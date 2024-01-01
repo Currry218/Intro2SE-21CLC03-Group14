a93f845fdbf6f187d54c994e28b6b99c83b0cd59
hoaithu842 <hoaithu842@gmail.com>
2024-01-01 09:57:20 +0700

    render info to layer 1 of guest homepage

--- src/controllers/guestController.js
+++ src/controllers/guestController.js
@@ -1,7 +1,57 @@
 const controller = {};
+const models = require("../models");
+const Sequelize = require('sequelize');
 
-controller.show = (req, res) => {
-	res.render('homepage', { title: "Trang chủ" , layout: "guestlayout", trangchu: true});
+controller.show = async (req, res) => {
+	const newbooks = await models.Book.findAll({
+		attributes: [
+		  "id",
+		  "title",
+		  "author",
+		  "imagePath",
+		  "price",
+		  "tags",
+		],
+		where: {
+			isVerified: true
+		},
+		order: [['updatedAt', 'DESC']], // Sorting by updatedAt in descending order
+		limit: 5
+	});
+
+	const trendingbooks = await models.Book.findAll({
+		attributes: [
+			"id",
+			"title",
+			"author",
+			"imagePath",
+			"price",
+			"tags",
+		],
+		where: {
+			isVerified: true
+		},
+		order: [[Sequelize.literal('ARRAY_LENGTH("buyer", 1)'), 'DESC']],
+		limit: 5
+	});
+
+	const allbooks = await models.Book.findAll({
+		attributes: [
+		  "id",
+		  "title",
+		  "author",
+		  "imagePath",
+		  "price",
+		  "tags",
+		],
+		where: {
+			isVerified: true
+		},
+		order: [['id', 'DESC']], // Sorting by updatedAt in descending order
+		limit: 5
+	});
+
+	res.render('guest_hp', { title: "Trang chủ" , layout: "guestlayout", trangchu: true, newbooks, trendingbooks, allbooks});
 }
 
 module.exports = controller;
